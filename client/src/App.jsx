447f6e35127ebdb9e272b5c7d4dac331b343c771
tomizdebski <tomizdebski@gmail.com>
2023-07-05 22:34:46 +0200

    add_components_login_register

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,19 +1,46 @@
 import {
   createBrowserRouter,
+  Outlet,
   RouterProvider,
 } from "react-router-dom";
-import './App.css'
+import './style.scss'
 import Register from './pages/Register'
-import Home from './pages/Home'
 import Login from './pages/Login'
+import Navbar from "./components/Navbar";
+import Home from "./pages/Home"
+import Footer from "./components/Footer";
 import Single from './pages/Single'
 import Write from './pages/Write'
+
+const Layout = () => {
+  return(
+    <>
+    <Navbar />
+    <Outlet />
+    <Footer />
+    </>
+  );
+};
 
 
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <Home />,
+    element: <Layout />,
+    children: [
+      {
+        path:"/",
+        element: <Home/>
+      },
+      {
+        path: "/post/:id",
+        element: <Single />,
+      },
+      {
+        path: "/write",
+        element: <Write />,
+      },
+    ]
   },
   {
     path: "/register",
@@ -23,25 +50,21 @@ const router = createBrowserRouter([
     path: "/login",
     element: <Login />,
   },
-  {
-    path: "/single",
-    element: <Single />,
-  },
-  {
-    path: "/write",
-    element: <Write />,
-  },
-
+  
 ]);
 
 function App() {
   
 
   return (
-    <>
-      <RouterProvider router={router} />
-    </>
+    <div className="app">
+      <div className="container">
+        <RouterProvider router={router} />
+      </div>
+    </div>
   )
 }
+
+
 
 export default App
